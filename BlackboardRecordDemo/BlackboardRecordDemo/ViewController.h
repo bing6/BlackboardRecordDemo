0671c9e020250dd6b391a4b9cff5f479bff0f2ec
Howard <bing.hao1982@gmail.com>
2015-09-07 17:38:19 +0800

    init

---
+++ BlackboardRecordDemo/BlackboardRecordDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlackboardRecordDemo
+//
+//  Created by bing.hao on 15/9/7.
+//  Copyright (c) 2015年 bing.hao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
